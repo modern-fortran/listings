799242f71b128f37e0e0172c8815f9de49073c6d
Izaak Beekman <zbeekman@gmail.com>
2017-12-04 12:48:28 -0500

    Fix unordered execution segs writing 2 same coarr

--- src/ch01/array_copy_caf.f90
+++ src/ch01/array_copy_caf.f90
@@ -18,7 +18,7 @@ write(*,'(a,i2,a,5(4x,i2))')'array on proc ', this_image(),&
 
 sync all
 
-array(:)[2] = array(:)[1]
+if (this_image() == 2) array(:) = array(:)[1]
 
 write(*,'(a,i2,a,5(4x,i2))')'array on proc ', this_image(),&
   ' after copy: ', array
