f2b03f2fdafb89f5790a13e239197cda087b5cb0
Milan Curcic <caomaco@gmail.com>
2018-10-22 09:02:16 -0400

    add hello derived types example

---
+++ src/ch06/hello_derived_types.f90
@@ -0,0 +1,19 @@
+module mod_person
+  type :: Person
+    character(len=10) :: name
+  contains  
+    procedure, public, pass(self) :: greet
+  end type Person
+contains
+  subroutine greet(self)
+    class(Person), intent(in) :: self
+    print *, 'Hello, my name is ' // trim(self % name) // '!'
+  end subroutine greet
+end module mod_person
+
+program hello_derived_types
+  use mod_person, only: Person
+  implicit none
+  type(Person) :: some_person = Person('Jill')
+  call some_person % greet()
+end program hello_derived_types
