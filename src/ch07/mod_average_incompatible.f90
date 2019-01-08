1d945c3507c0a1732fd06f9eb113971568d53df6
Milan Curcic <caomaco@gmail.com>
2019-01-08 00:43:11 -0500

    add the incompatible types example

---
+++ src/ch07/mod_average_incompatible.f90
@@ -0,0 +1,21 @@
+module mod_average
+
+  implicit none
+
+  private
+  public :: average
+
+contains
+
+  pure real function average(x) result(res)
+   real, intent(in) :: x(:)
+   res = sum(x) / size(x)
+  end function average
+
+end module mod_average
+
+
+program test_average
+  use mod_average, only: average
+  print *, average([1, 6, 4])
+end program test_average
