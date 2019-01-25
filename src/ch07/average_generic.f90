c2056d9d6a73d3732f9274c5fc3d165515a934e7
Milan Curcic <caomaco@gmail.com>
2019-01-25 10:01:55 -0500

    add average_field() implementation in support of Exercise 1

--- src/ch07/average_generic.f90
+++ src/ch07/average_generic.f90
@@ -1,4 +1,19 @@
+module mod_field
+
+  implicit none
+
+  private
+  public :: Field
+
+  type :: Field
+    real, allocatable :: data(:)
+  end type Field
+
+end module mod_field
+
 module mod_average
+
+  use mod_field, only: Field
 
   implicit none
 
@@ -6,7 +21,10 @@ module mod_average
   public :: average
 
   interface average
-    module procedure :: average_int, average_real, average_logical
+    module procedure :: average_int
+    module procedure :: average_real
+    module procedure :: average_logical
+    module procedure :: average_field
   end interface average
 
 contains
@@ -26,12 +44,26 @@ contains
     res = real(count(x), kind=kind(res)) / size(x)
   end function average_logical
 
+  pure real function average_field(f) result(res)
+    class(Field), intent(in) :: f
+    res = average(f % data)
+  end function average_field
+
 end module mod_average
 
 
 program test_average
+
   use mod_average, only: average
+  use mod_field, only: field
+
+  type(Field) :: f
+
   print *, average([1, 6, 4])
   print *, average([1., 6., 4.])
   print *, average([.true., .true., .false.])
+
+  f % data = [1., 6., 4.]
+  print *, average(f)
+
 end program test_average
