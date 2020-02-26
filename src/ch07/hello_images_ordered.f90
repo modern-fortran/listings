59374da6cae0a95fb308249f807b5983f3604323
Milan Curcic <caomaco@gmail.com>
2020-02-26 16:40:35 -0500

    rename program

--- src/ch07/hello_images_ordered.f90
+++ src/ch07/hello_images_ordered.f90
@@ -1,4 +1,4 @@
-program hello_images_syncall
+program hello_images_ordered
   ! Synchronizing parallel images to print in order.
   implicit none
   integer :: n
@@ -7,4 +7,4 @@ program hello_images_syncall
       print *, 'Hello from image', this_image(), 'of', num_images()
     sync all
   end do
-end program hello_images_syncall
+end program hello_images_ordered
