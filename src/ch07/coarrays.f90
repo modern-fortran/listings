b2b1d921275040060a922cf4bf271ad5593a5e1b
Milan Curcic <caomaco@gmail.com>
2020-03-24 19:47:57 -0400

    remove periods at the end of the output for consistency with the book style

--- src/ch07/coarrays.f90
+++ src/ch07/coarrays.f90
@@ -12,7 +12,7 @@ program coarrays
   if (this_image() == 1) then
     a = 1
     print *, 'Image ', this_image(), ' has value ', a
-    print *, 'Image ', this_image(), ' sending new value to image 2.'
+    print *, 'Image ', this_image(), ' sending new value to image 2'
     a[2] = 2 * a
   end if
 
@@ -20,7 +20,7 @@ program coarrays
 
   if (this_image() == 2) then
     print *, 'Image ', this_image(), ' has value ', a
-    print *, 'Image ', this_image(), ' sending new value to image 1.'
+    print *, 'Image ', this_image(), ' sending new value to image 1'
     a[1] = 2 * a
   end if
 
